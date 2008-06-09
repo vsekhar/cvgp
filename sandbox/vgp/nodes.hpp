a329bb2cb9a0d5f364e6ade6bb579efa0d0154f4
Vivek Sekhar <vsekhar@xmain.com>
2008-06-09 00:00:49 +0000

    Deleting vgp/sandbox
    git-svn-id: file:///home/vsekhar/vgp/vgp@56 cb3134f3-094e-0410-9174-ff1f5a1024ca

--- sandbox/vgp/nodes.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef NODES_HPP_
-#define NODES_HPP_
-
-#include <vgp/nodes/node.hpp>
-#include <vgp/nodes/terminal.hpp>
-#include <vgp/nodes/adapters.hpp>
-#include <vgp/nodes/nodeloader.hpp>
-
-#endif /*NODES_HPP_*/
--- sandbox/vgp/util.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef UTIL_HPP_
-#define UTIL_HPP_
-
-#include <vgp/util/typeinfo.hpp>
-#include <vgp/util/output.hpp>
-#include <vgp/util/gettypes.hpp>
-
-#endif /*UTIL_HPP_*/
